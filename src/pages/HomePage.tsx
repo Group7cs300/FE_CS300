a59a3b079ae0c5061a55ac863d00c9aca5b278bb
Phuc-eoh <114310451+phuc-lychee@users.noreply.github.com>
2022-10-28 20:18:26 +0700

    .

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap'
+import { Navbar } from '../components'
+
+export default function HomePage() {
+	return (
+		<Container>
+			<Navbar />
+			<h1>Home</h1>
+		</Container>
+	)
+}
