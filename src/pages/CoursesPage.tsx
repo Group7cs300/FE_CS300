9f875bbc2a4230b29fbf5843791e6374db825813
Phuc <ttphuc18@apcs.fitus.edu.vn>
2022-11-26 10:57:31 +0700

    Squashed commit of the following:
    commit 62a9118c0389099f0ad903a8e383e4e674364637
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Sat Nov 26 10:44:28 2022 +0700
    
        searchResultPage
    
    commit 137c2ff8a96610178a615faaac1a2adb991f9206
    Merge: 32578f3 427fa75
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Sat Nov 26 10:16:50 2022 +0700
    
        Merge branch 'main' into I2G-41-search-result-page
    
    commit 32578f3873658d8f53f9c2031aa41db011b3b079
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Sat Nov 19 15:57:34 2022 +0700
    
        [I2G-41] SearchResultPage

--- src/pages/CoursesPage.tsx
+++ src/pages/CoursesPage.tsx
@@ -1,27 +1,43 @@
 import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Stack } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import client from '../client/axios'
+import CourseGrid from '../components/courses_grid/CourseGrid'
 import { Footer, Navbar } from '../container'
+import Filter from '../container/SearchResults/SearchResults'
+import { CountResults } from '../container/SearchResults/SearchResults'
 
 export default function CoursesPage() {
 	interface Course {
 		uuid: number
 		name: string
 	}
-
 	const { search_text } = useParams()
+	const [page, setPage] = useState(1)
 	const [courses, setCourses] = useState<Course[]>([])
+	const [count, setCount] = useState(0)
+	// useEffect(() => {
+		
+	// }, [])
 
 	useEffect(() => {
-		async function fetchCourses() {
-			const { data } = await client.get('/course')
-			setCourses(data)
+		async function fetchCourses(page: number) {
+			const { data } = await client.get(`/course?page=${page}&limit=9`)
+			setCount(data.length)
+			let grid: any[] = []
+			for (const [index, course] of data.entries()) {
+				if (index == grid.length * 3) grid.push([])
+				grid[grid.length - 1].push(course)
+			}
+			for (let i = grid[grid.length - 1].length; i < 3; ++i)
+				grid[grid.length - 1].push('')
+			console.log(grid)
+			setCourses(grid)
 		}
 
-		fetchCourses()
-	}, [])
-
+		fetchCourses(page)
+	}, [page])
+	console.log(page)
 	return (
 		<div
 			style={{
@@ -32,12 +48,13 @@ export default function CoursesPage() {
 		>
 			<Navbar />
 			<Container style={{ flex: 1 }}>
-				<h1>Search text: {search_text}</h1>
-				<div>
-					{courses.map((course) => (
-						<div>{course.name}</div>
-					))}
+				<CountResults search_text={search_text} count={count} />
+				<hr />
+				<div className='container d-flex flex-row justify-content-between'>
+					<Filter/>
+					<CourseGrid courses={courses} page={page} setPage={setPage}/>
 				</div>
+				<hr />
 			</Container>
 			<Footer />
 		</div>
