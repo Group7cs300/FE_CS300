17eb107968ce7b4b2188c493b3cc00125f4ed5bc
Phuc <ttphuc18@apcs.fitus.edu.vn>
2022-12-07 16:59:42 +0700

    Update CourseGrid.tsx

--- src/containers/courses/CourseGrid.tsx
+++ src/containers/courses/CourseGrid.tsx
@@ -1,42 +1,30 @@
-import { Col, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import { PageNumber } from '../../components/courses/PageNumber'
 import CourseCard from '../../components/courses/CourseCard'
-import { useEffect, useState } from 'react'
 
 export default function CourseGrid({
-	props,
 	courses,
 	totalCourses,
 	page,
 	setPage,
 }: any) {
-	const [displayCourses, setDisplayCourses] = useState([])
-
-	useEffect(() => {
-		let grid: any = []
-
-		while (courses.length > 0) grid.push(courses.splice(0, 3))
-		setDisplayCourses(grid)
-	}, [courses])
 	return (
 		<div className="col col-xxl-9 col-xl-7 col-lg-8 col-sm-6 col-md-6">
 			<div>
-				{displayCourses.map((rows: any, id: any) => {
-					return (
-						<Row sm="1" lg="2" xxl="3" key={id}>
-							{rows.map((course: any, id: any) => (
-								<Col className="p-4" key={id}>
-									<CourseCard course={course} />
-								</Col>
-							))}
-						</Row>
-					)
-				})}
+				<Container>
+					<Row sm="1" lg="2" xxl="3">
+						{courses.map((course: any) => (
+							<Col className="p-4" key={course.name}>
+								<CourseCard course={course} />
+							</Col>
+						))}
+					</Row>
+				</Container>
 			</div>
 			<div>
 				<PageNumber
 					page={page}
-					totalPage={Math.ceil(totalCourses/9)}
+					totalPage={Math.ceil(totalCourses / 9)}
 					setPage={setPage}
 				></PageNumber>
 			</div>
