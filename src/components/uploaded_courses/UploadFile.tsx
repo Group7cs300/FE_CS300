f9d259935a41f00436cf1ac0e28d3b46e49b4cb6
tphat2001 <81370495+tphat2001@users.noreply.github.com>
2023-01-08 11:16:21 +0700

    Update UploadFile.tsx

--- src/components/uploaded_courses/UploadFile.tsx
+++ src/components/uploaded_courses/UploadFile.tsx
@@ -36,17 +36,12 @@ export default function UploadFile({
 		},
 
 		beforeUpload(file, FileList) {
-			const isPNG = file.type === 'image/png';
-    		if (!isPNG) {
-      		message.error(`${file.name} is not a png file`);
-    			}
-			else{
 			setIsUpload(!upload)
 			setUpload(!upload)
 			setFiles({
 				file: FileList,
 				id: id - 1,
-			})}
+			})
 
 			return false
 		},
